0b12dceeec8a25cabbc547763fbaa27b7f77d6c8
Bora Yalçın <borantula@users.noreply.github.com>
2018-08-13 10:53:46 +0200

    details of revenue added

--- src/components/movie/Detail.js
+++ src/components/movie/Detail.js
@@ -1,16 +1,22 @@
 import React from "react";
+import "./Detail.css";
 
 const MovieDetail = ({ movie }) => {
   return (
-    <div>
-      <h1>{movie.title}</h1>
+    <div className="movie-detail">
+      <h4>{movie.tagline}</h4>
+      <h1 className="movie-detail__title">{movie.title}</h1>
+
       <img
         className="movie-item__poster"
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         width="150"
         alt={movie.title}
       />
-      <div className="movie-item__desc">{movie.overview}</div>
+      <div>Budget: ${movie.budget.toLocaleString("en")}</div>
+      <div>Revenue: ${movie.revenue.toLocaleString("en")}</div>
+      <div> {movie.runtime} min.</div>
+      <div className="movie-detail__desc">{movie.overview}</div>
     </div>
   );
 };
