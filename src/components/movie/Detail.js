032884c66e6778b52051e4b2185cce1e6363f6b5
Bora Yalcin <email@borayalcin.me>
2018-08-19 12:34:49 +0200

    proptype to movie detail added

--- src/components/movie/Detail.js
+++ src/components/movie/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./Detail.css";
 
 const MovieDetail = ({ movie }) => {
@@ -24,6 +25,10 @@ const MovieDetail = ({ movie }) => {
       />
     </div>
   );
+};
+
+MovieDetail.propTypes = {
+  movie: PropTypes.object.isRequired
 };
 
 export default MovieDetail;
