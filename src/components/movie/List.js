c8e67cd5803426f2f320edb488cacb56cd89ec2a
Bora Yalcin <email@borayalcin.me>
2018-09-26 22:03:48 +0200

    create react app 2.0.0 added

--- src/components/movie/List.js
+++ src/components/movie/List.js
@@ -1,19 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import MovieListItem from "../movie/ListItem";
-//import MovieListItemAd from "../movie/ListItemAd";
+import MovieListItemAd from "../movie/ListItemAd";
 import "./List.css";
 
 const MovieList = ({ movies = [], savedMovies }) => {
-  //  let counter = 0;
+  let counter = 0;
   return (
     <div className="movie-list">
       {movies &&
         movies.map(movie => {
-          //counter++;
+          counter++;
           return (
             <React.Fragment key={movie.id}>
               <MovieListItem movie={movie} />
+              {counter % 5 === 0 && <MovieListItemAd />}
             </React.Fragment>
           );
         })}
