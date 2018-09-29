b632ac213c8fdde5136748fa86d171c0ea0cadee
Bora Yalcin <email@borayalcin.me>
2018-09-29 09:11:08 +0200

    history added

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { createBrowserHistory } from "history";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { ConnectedRouter } from "connected-react-router";
 //import { createLogger } from "redux-logger";
-import reducer from "./reducers";
+import rootReducer from "./reducers";
 import App from "./App";
 import "./styles.css";
 
-const middleware = [thunk];
+const history = createBrowserHistory();
+
+const middleware = [routerMiddleware(history), thunk];
 if (process.env.NODE_ENV !== "production") {
   //middleware.push(createLogger());
 }
 
 const store = createStore(
-  reducer,
+  connectRouter(history)(rootReducer),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(...middleware)
+  compose(applyMiddleware(...middleware))
 );
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
